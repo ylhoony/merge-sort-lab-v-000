4fb4be46f2d4d5076b28c81a798c46a801b09f5c
Hoon Lee <yl.hoony@gmail.com>
2018-08-23 00:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ function findMinAndRemoveSorted(arr) {
 }
 
 function merge(firstArr, secondArr) {
+  let = firstArr[0]
+  let secondArr[0]
+  
+  while (firstArr.length !== 0 && secondArr.length !== 0) {
+    if 
+  }
   
 }
 
