82e89976e471aa436eb5625f63b558244e1ad752
Hoon Lee <yl.hoony@gmail.com>
2018-08-23 00:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ function mergeSort(array) {
   let midpoint = array.length/2
   let firstHalf = array.slice(0, midpoint)
   let secondHalf = array.slice(midpoint, array.length)
+  let sorted;
  
   if(array.length < 2){
     return array
   } else {
-    let sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    sorted = merge(mergeSort(firstHalf), mergeSort(secondHalf))
   }
   return sorted
 }
