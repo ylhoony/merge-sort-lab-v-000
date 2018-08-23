945a72a4b75d46d2eb84c4d1efb6c856aaf6a845
Hoon Lee <yl.hoony@gmail.com>
2018-08-23 00:36:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 function findMinAndRemoveSorted (arr) {
   let min;
   
-  arr.forEach((num) => {
-min    if (min === undefined || num <= min) {
-      min = num
-    })
-  }
-  return 
+
 }
 
 function merge(firstArr, secondArr) {
